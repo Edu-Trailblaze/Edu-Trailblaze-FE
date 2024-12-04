a5c12288494b0e2df5934194cf22b62a4e49b1d2
LeThanhNhan_9104 <lethanhnhan09012004@gmail.com>
2024-12-04 18:05:27 +0700

    Upload full content of EduTrailBlaze-FE without submodule

---
+++ EduTrailBlaze-FE/edutrailblaze/src/app/page.tsx
@@ -0,0 +1,10 @@
+import WebHeader from "@/components/header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <WebHeader></WebHeader>
+    </div>
+  );
+}
