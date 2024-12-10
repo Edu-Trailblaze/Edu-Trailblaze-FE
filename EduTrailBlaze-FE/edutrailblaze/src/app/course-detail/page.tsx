c6e178e084efbecf49c6d60b3e870bdbb6ea2b1a
ttd112004 <dat.tran@richpaymentsolutions.com>
2024-12-10 10:57:38 +0700

    clean code

--- EduTrailBlaze-FE/edutrailblaze/src/app/course-detail/page.tsx
+++ EduTrailBlaze-FE/edutrailblaze/src/app/course-detail/page.tsx
@@ -2,11 +2,8 @@
 import CourseAbout from '../../components/course-detail/course_about';
 import CourseDetails from '../../components/course-detail/course_details';
 import CourseHeader from '../../components/course-detail/course_header';
-import WebFooter from '../../components/footer';
-import WebHeader from '../../components/header';
 
 export default function Blog() {
-  // this is a server component
   return (
     <div>
       <CourseHeader/>
--- EduTrailBlaze-FE/edutrailblaze/src/components/course-detail/course_details.tsx
+++ EduTrailBlaze-FE/edutrailblaze/src/components/course-detail/course_details.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 export default function CourseDetails() {
   const [selected, setSelected] = useState<string>('About');
+  const items = ['About', 'Outcomes', 'Courses', 'Testimonials'];
 
   const handleClick = (items: string) => {
     setSelected(items);
@@ -11,46 +12,18 @@ export default function CourseDetails() {
   return (
     <div className='container mt-8 mb-10 '>
       <ul className='flex gap-10 w-[900px] border-b-2'>
-        <li>
-          <button
-            onClick={() => handleClick('About')}
-            className={`${
-              selected === 'About' ? 'text-blue-500 bg-blue-100' : ''
-            } px-4 py-2 rounded-lg mb-2`}
-          >
-            About
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => handleClick('Outcomes')}
-            className={`${
-              selected === 'Outcomes' ? 'text-blue-500 bg-blue-100' : ''
-            } px-4 py-2 rounded-lg`}
-          >
-            Outcomes
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => handleClick('Courses')}
-            className={`${
-              selected === 'Courses' ? 'text-blue-500 bg-blue-100' : ''
-            } px-4 py-2 rounded-lg`}
-          >
-            Courses
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => handleClick('Testimonials')}
-            className={`${
-              selected === 'Testimonials' ? 'text-blue-500 bg-blue-100' : ''
-            } px-4 py-2 rounded-lg`}
-          >
-            Testimonials
-          </button>
-        </li>
+        {items.map((option) => (
+          <li key={option}>
+            <button
+              onClick={() => handleClick(option)}
+              className={`${
+                selected === option ? 'text-blue-500 bg-blue-100' : ''
+              } px-4 py-2 rounded-lg mb-2`}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
