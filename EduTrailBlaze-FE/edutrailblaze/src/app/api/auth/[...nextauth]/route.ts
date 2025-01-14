4e04f73f93f589b8ad1a5c062f705b1d1a4f5111
LeThanhNhan_9104 <lethanhnhan09012004@gmail.com>
2025-01-14 10:47:12 +0700

    integrate next auth for login

---
+++ EduTrailBlaze-FE/edutrailblaze/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook"
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Missing email or password");
+        }
+        // Replace with actual authentication logic
+        return { id: "1", name: "User", email: credentials.email };
+      },
+    }),
+  ],
+  debug: true,
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
