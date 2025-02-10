f215f8786d115e3baf4e7578851bc55658d2ecba
LeThanhNhan_9104 <lethanhnhan09012004@gmail.com>
2025-02-10 20:58:07 +0700

    Temporarily complete the login, register function

---
+++ EduTrailBlaze-FE/src/redux/slice/auth.slice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface AuthState {
+  accessToken: string | null
+  refreshToken: string | null
+  twoFactorCode: string | null
+}
+
+const initialState: AuthState = {
+  accessToken: null,
+  refreshToken: null,
+  twoFactorCode: null,
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action: PayloadAction<{ accessToken: string; refreshToken: string }>) => {
+      state.accessToken = action.payload.accessToken
+      state.refreshToken = action.payload.refreshToken
+      localStorage.setItem('accessToken', action.payload.accessToken)
+      localStorage.setItem('refreshToken', action.payload.refreshToken)
+    },
+    logout: (state) => {
+      state.accessToken = null
+      state.refreshToken = null
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken') 
+    },
+  },
+})
+const authReducer = authSlice.reducer
+
+export const { setCredentials, logout } = authSlice.actions
+export default authReducer
--- EduTrailBlaze-FE/src/redux/store.ts
+++ EduTrailBlaze-FE/src/redux/store.ts
@@ -6,6 +6,17 @@ import type { TypedUseSelectorHook } from 'react-redux'
 import { useDispatch, useSelector } from 'react-redux'
 import courseReducer from './slice/course.slice'
 import { courseApi } from '../services/course.service'
+import authReducer from './slice/auth.slice' // Import auth slice
+import { authApi } from '@/services/auth.service'
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from 'redux-persist'
+
+const persistConfig = {
+  key: 'auth',
+  storage
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
 
 export const store = configureStore({
   reducer: {
@@ -13,12 +24,21 @@ export const store = configureStore({
 
     //createSlice
     course: courseReducer,
+    auth: persistedAuthReducer,
 
     //createApi
-    [courseApi.reducerPath]: courseApi.reducer
+    [courseApi.reducerPath]: courseApi.reducer,
+    [authApi.reducerPath]: authApi.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(courseApi.middleware)
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false // Bỏ kiểm tra serialize để tránh lỗi
+    }).concat(courseApi.middleware, authApi.middleware)
 })
+
+export const persistor = persistStore(store);
+
+setupListeners(store.dispatch)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
@@ -26,3 +46,4 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
