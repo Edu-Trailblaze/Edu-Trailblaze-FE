9db19dd318343ca835736644f92be62c857e94a3
LeThanhNhan_9104 <lethanhnhan09012004@gmail.com>
2025-02-15 22:35:26 +0700

    finish cart feature

---
+++ EduTrailBlaze-FE/src/redux/slice/cart.slice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+ // Ensure correct import
+
+const initialState: ICart = {
+  cartItems: [],
+  totalPrice: 0,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItemToCart: (state, action: PayloadAction<CartItem>) => {
+      console.log('addItemToCart payload:', action.payload); // Log the payload
+      state.cartItems.push(action.payload);
+      state.totalPrice += action.payload.totalCoursePrice;
+    },
+    setCart: (state, action: PayloadAction<ICart>) => {
+      console.log('setCart payload:', action.payload); // Log the payload
+      state.cartItems = action.payload.cartItems;
+      state.totalPrice = action.payload.totalPrice;
+    },
+  },
+});
+
+export const { addItemToCart, setCart } = cartSlice.actions;
+export default cartSlice.reducer;
