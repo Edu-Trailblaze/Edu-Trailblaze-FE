86fdef2814ea40da5108f26aee2de635fc97920a
ttd11204 <datkk112004@gmail.com>
2025-03-03 12:05:16 +0700

    config next

--- EduTrailBlaze-FE/next.config.ts
+++ EduTrailBlaze-FE/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
+  distDir: '.next',
+  output: 'standalone',
   images: {
     domains: ['images.pexels.com']
   }
