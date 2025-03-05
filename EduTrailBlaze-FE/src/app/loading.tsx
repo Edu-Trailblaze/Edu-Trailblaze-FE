fee9a700f5d9cd5b8ee8507b8c0b344f7777b4ab
LeThanhNhan_9104 <lethanhnhan09012004@gmail.com>
2025-03-05 08:35:14 +0700

    add loading between route in app

---
+++ EduTrailBlaze-FE/src/app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingPage from '@/components/animate/Loading/LoadingPage';
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <LoadingPage />
+    </div>
+  );
+}
