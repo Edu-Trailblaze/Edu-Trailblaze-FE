70c211e4fcd4499745be5ff206907312451a05ee
ttd11204 <datkk112004@gmail.com>
2025-03-08 18:21:11 +0700

    fix lecture reading

--- EduTrailBlaze-FE/src/components/user/learning/lecture/content/reading/lecture_reading.tsx
+++ EduTrailBlaze-FE/src/components/user/learning/lecture/content/reading/lecture_reading.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import { Download } from 'lucide-react'
 
 interface ReadingLectureProps {
   lecture: ILecture
@@ -12,23 +11,17 @@ export default function ReadingLecture({ lecture }: ReadingLectureProps) {
       <p className='text-3xl font-bold'>{lecture.title}</p>
 
       <div className='bg-white rounded-xl border-2 border-blue-500 shadow-sm p-6'>
-        <p className='text-gray-700 leading-relaxed'>{lecture.content}</p>
+        {lecture.content.split('\n').map((line, index) => (
+          <p key={index} className='text-gray-700 leading-relaxed'>
+            {line}
+          </p>
+        ))}
       </div>
 
       {/* Danh sách file đọc */}
       <div className='mt-8'>
         <h2 className='text-xl font-semibold mb-4'>Document</h2>
         <p className='text-gray-700 leading-relaxed'>{lecture.description}</p>
-        <ul className='space-y-3'>
-          {lecture.content.split(',').map((file: any, index: any) => (
-            <li key={index} className='flex items-center gap-3'>
-              <Download className='w-5 h-5 text-blue-600' />
-              <a href={file.trim()} download className='text-blue-600 hover:underline'>
-                {file.split('/').pop()}
-              </a>
-            </li>
-          ))}
-        </ul>
       </div>
     </div>
   )
