1b5342cfa63670525b8d0021b1cfcef6304620ef
ttd11204 <datkk112004@gmail.com>
2025-03-17 14:46:35 +0700

    fix hard code course in new section

--- EduTrailBlaze-FE/src/components/user/instructor/createLecture/CreateLecture.tsx
+++ EduTrailBlaze-FE/src/components/user/instructor/createLecture/CreateLecture.tsx
@@ -223,7 +223,13 @@ export default function CreateLecture({ sectionId }: LectureInfoProps) {
                           type='button'
                           className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-lg shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150'
                         >
-                          {addLectureLoading ? <ClipLoader /> : <><Check /> Create Now</>}
+                          {addLectureLoading ? (
+                            <ClipLoader />
+                          ) : (
+                            <>
+                              <Check /> Create Now
+                            </>
+                          )}
                         </button>
                       </div>
                     </div>
