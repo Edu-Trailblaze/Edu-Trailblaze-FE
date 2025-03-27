2931005aac164a906a7dcc89a22ce345eb8e66a5
ttd11204 <datkk112004@gmail.com>
2025-03-27 11:45:39 +0700

    call api again when enroll

--- EduTrailBlaze-FE/src/redux/services/enroll.service.ts
+++ EduTrailBlaze-FE/src/redux/services/enroll.service.ts
@@ -6,16 +6,16 @@ export const enrollApi = createApi({
   reducerPath: 'Enrollment/api',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL
-  }), 
+  }),
   tagTypes: ['Enrolls'],
   endpoints: (build) => ({
-
     getCheckCourseStatus: build.query<EnrollCourseStatus, Omit<EnrollCourseStatus, 'status'>>({
       query: ({ courseId, studentId }) => ({
         url: `Enrollment/check-course-status`,
         method: 'GET',
         params: { courseId, studentId }
-      })
+      }),
+      providesTags: ['Enrolls']
     }),
 
     postEnroll: build.mutation<any, PostEnroll>({
@@ -23,20 +23,25 @@ export const enrollApi = createApi({
         url: `Enrollment`,
         method: 'POST',
         body
-      })
+      }),
+      invalidatesTags: ['Enrolls']
     }),
 
     //new
     getStudentCourseProgress: build.query<Progress, { studentId: string; courseId: number }>({
-    query: ({ studentId, courseId }) => ({
-      url: `Enrollment/student-course-progress`,
-      method: 'GET',
-      // Chú ý viết hoa chữ "S" và "C" giống như Swagger yêu cầu
-      params: { StudentId: studentId, CourseId: courseId }
+      query: ({ studentId, courseId }) => ({
+        url: `Enrollment/student-course-progress`,
+        method: 'GET',
+        // Chú ý viết hoa chữ "S" và "C" giống như Swagger yêu cầu
+        params: { StudentId: studentId, CourseId: courseId }
       })
     })
-
   })
 })
 
-export const { useGetCheckCourseStatusQuery, usePostEnrollMutation, useLazyGetCheckCourseStatusQuery, useGetStudentCourseProgressQuery } = enrollApi
+export const {
+  useGetCheckCourseStatusQuery,
+  usePostEnrollMutation,
+  useLazyGetCheckCourseStatusQuery,
+  useGetStudentCourseProgressQuery
+} = enrollApi
